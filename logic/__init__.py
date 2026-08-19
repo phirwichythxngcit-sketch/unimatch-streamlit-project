c518f5c8263230a8c0fd7bafccf42c1926249279
peerawit thongjit <phirwichythxngcit@gmail.com>
2026-08-19 23:57:31 +0700

    Update __init__.py

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,1 +1,7 @@
+from logic.mbti_engine import MBTIEngine
+from logic.scoring import ScoringEngine
+from logic.logic_engine import LogicEngine
+from logic.faculty_matcher import FacultyMatcher
+from logic.database import Database
 
+__all__ = ["MBTIEngine", "ScoringEngine", "LogicEngine", "FacultyMatcher", "Database"]
